388e75c8be380c7ed48ee23776eaff1f28e5d860
Juri <juri@jutametall.ee>
2022-03-01 17:26:07 +0200

    AD_script_task+aduser+checkgroup

---
+++ AD_script_task_plus_addgroup2_func.ps1
@@ -0,0 +1,33 @@
+﻿$data=Import-Csv "C:\Users\Administrator\Downloads\adusers.csv" -Header username,fullname,password,company,phone,city,department,dname
+foreach ($user in $data){
+       $name2=$user.username
+       $adusername=Get-ADUser -Filter 'Name -eq $name2'  
+         if (($adusername)){                    
+                    Write-Host "User with name " $user.username " allredy exists" -ForegroundColor Green
+                    Write-Host "Would you like to delete a user with name: " $user.username "?" -ForegroundColor Red
+                        $yn=Read-Host "Enter some value: y/n "
+                            switch($yn){
+                                "y" {
+                                    Remove-ADUser $user.username
+                                    Write-Host "User with name " $user.username " - was removed" -ForegroundColor Red
+                                    }
+
+                                 default{
+                                    Write-Host  "User with name " $user.username " stayed in the domain" -ForegroundColor Green
+
+#--------------------------------------------------------------------------start------group validation code and adding user to group Active Directory--------------------------------------------------
+                                     C:\Users\Administrator\Downloads\functionGetGroupAD.ps1
+#==========================================================================stop-------group validation code and adding user to group Active Directory--------------------------------------------------
+                                   }        
+                            }            
+                        }else{
+                               $password=ConvertTo-SecureString $user.password -AsPlainText -Force
+                                New-ADUser -Name $user.username -Surname $user.fullname -AccountPassword $password -PasswordNeverExpires $true -Company $user.company -OfficePhone $user.phone -City $user.city -Department $user.department -CannotChangePassword $true -Enabled $true
+                                Set-ADUser $user.username -Enabled $true
+                                Write-Host "User "$user.username" was added!" -ForegroundColor Green
+                                       
+#--------------------------------------------------------------------------start------group validation code and adding user to group Active Directory--------------------------------------------------
+                                     C:\Users\Administrator\Downloads\functionGetGroupAD.ps1
+#============================================================================stop-------group validation code and adding user to group Active Directory--------------------------------------------------
+                }
+            }
---
+++ functionGetGroupAD.ps1
@@ -0,0 +1,41 @@
+﻿#--------------------------------------------------------------------------start------group validation code and adding user to group Active Directory--------------------------------------------------
+                         # function GetGroupAD  {}
+                                Write-Host "Would you like to add user" $user.username "to a grupp named" $user.department " ?" -ForegroundColor Red
+                                    $yn=Read-Host "Enter some value: y/n "
+                                    switch($yn){
+                                        "y" {                                                                      
+                                   $gruppname=$user.department
+                                   $adgruppname=Get-ADGroup -filter 'SamAccountName -eq $gruppname'
+
+                                     if (($adgruppname)){
+                    
+                                                Write-Host "Grupp with name " $gruppname " allredy exists_01" -ForegroundColor Green
+                                                Add-ADGroupMember -Identity $gruppname -Members $name2
+                                                Write-Host "User with name " $user.username " nas been added to an existing group" $gruppname -ForegroundColor Green
+
+                                            }else{
+                                               Write-Host "Grupp with name " $gruppname " not created" -ForegroundColor Red
+                                               Write-Host "Would you like to create a grupp with name: " $gruppname "?" -ForegroundColor Red
+                  
+                                              $yn=Read-Host "Enter some value: y/n "
+                                                switch($yn){
+                                                    "y" {
+                                                         New-ADGroup -GroupScope Domainlocal -Name $gruppname
+                                                         Write-Host "Grupp with name " $gruppname " was created" -ForegroundColor Green
+                                                         Add-ADGroupMember -Identity '$gruppname' -Members '$user.username'
+                                                         Write-Host "User with name " $user.username " nas been added into the newly created group" $gruppname -ForegroundColor Green
+                                                        }
+
+                                                     default{
+                                                        Write-Host "Grupp with name " $gruppname " not created" -ForegroundColor Red
+                                                        Write-Host "User with name " $user.username " user was not added to the group" $gruppname "due to its asence" -ForegroundColor Green
+                                                       }        
+                                                     }                  
+                                                  }
+                                           }
+                                    default{
+                                            Write-Host "User with name " $user.username " user was not added to the group" $gruppname -ForegroundColor Red
+                                            }
+                            }
+                            
+#==========================================================================stop-------group validation code and adding user to group Active Directory--------------------------------------------------
